96194306599d9e8453f4bdd28d458467f1a89f9a
YasmiinAli <yasmin.ali.helmy@gmail.com>
2023-10-24 00:43:43 +0300

    add

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 
-export default function TaskCard({task}) {
-    console.log(task);
+export default function TaskCard({task, handleDelete, handleEdit}) {
+    // console.log(task);
   return (
     <div >
-        <div className="card " >
+        <div className={`card  ${task.status ==='Not Started' ? 'bg-warning': 
+        task.status ==='In progress' ?'bg-primary':'bg-success'}  
+        `} >
   <div className="card-body">
-    <h5 className="card-title"> status:{task.status}</h5>
+    <h5 className="card-title"> status: {task.status}</h5>
     <p className="card-text">{task.description}</p>
     <div className='mx-0'>
-        <i className="fa-solid fa-pen-to-square m-1 text-info cursor-pointer"></i>
-        <i className="fa-solid fa-trash text-danger cursor-pointer"></i>
+        <i className="fa-solid fa-pen-to-square m-1 text-info cursor-pointer" onClick={()=>handleEdit()}></i>
+        <i className="fa-solid fa-trash text-danger cursor-pointer" onClick={()=>handleDelete(task.id)} ></i>
     </div>
   </div>
 </div>
+
     </div>
   )
 }
--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,57 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TaskCard from '../TaskCard/TaskCard'
 
 export default function Tasks() {
-    const listofTasks =[
-        {description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
+    let listofAllTasks =[
+        {id:1,
+            description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
         status:'Finished'
     },
-    {description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
+    {id:2,
+        description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
         status:'Not Started'
     },
-    {description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
+    {id:3,
+        description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
         status:'In progress'
     },
-    {description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
+    {id:4,
+        description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
         status:'Finished'
     },
-{description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
+{id:5,
+    description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
         status:'In progress'
     },
-    {description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
+    {id:6,
+        description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
         status:'In progress'
     },
-    {description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
+    {id:7,
+        description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
         status:'Not Started'
     },
-    {description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
+    {id:8,
+        description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
         status:'Not Started'
     },
-    {description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
+    {id:9,
+        description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
         status:'Finished'
     },
-    {description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
+    {id:10,
+        description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
         status:'Not Started'
     },
-    {description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
+    {id:11,
+        description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
         status:'In progress'
     },
-    {description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
+    {id:12,
+        description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
         status:'Not Started'
     },
-    {description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
+    {id:13,
+        description :'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.',
         status:'Finished'
     },
     ]
-    
+        let [listofTasks,setListOfTasks] = useState(listofAllTasks)
+
+    const handleDelete = (id) => {
+        listofTasks=listofTasks.filter((task)=> task.id !== id)
+        setListOfTasks(listofTasks)
+        // console.log( id ,'deleted')
+    }
+    const handleEdit = () => {
+
+    }
+    console.log(listofTasks);
   return (
         <div className="container m-5">
         <div className="row">
             <h2 className='text-info'>Tasks</h2>
-           { listofTasks.map((task,index) =>
+           { listofTasks?.map((task,index) =>
             (<>
             <div className='col-md-6 mb-2' key={index}>
-<TaskCard task ={task}/>
+<TaskCard task ={task} handleDelete={handleDelete}  handleEdit={handleEdit} />
             </div>
             </>))}
                 
